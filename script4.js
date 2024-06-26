4aed682689128da2f7647b33a229bde90aca2e5e
EduardaBorgesD <medafbo@gmail.com>
2024-06-27 00:22:14 +0100

    AC Tentativa Background

--- script4.js
+++ script4.js
@@ -1,25 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sheep = document.getElementById('sheep');
     let sheepTimer;
-
+    let sheepsClicked = 0;
+    const totalSheep = 15;
+    const gameContainer = document.querySelector('.container-sheep');
     const video = document.querySelector("#in-video video");
     const gamedisplay = document.getElementById("gamedisplay");
 
     if (video) {
         video.addEventListener('ended', () => {
             gamedisplay.style.display = 'block';
-            video.style.display= 'none';
+            video.style.display = 'none';
             moveSheep();
         });
     }
 
-  function randomPosition() {
-        const gameContainer = document.querySelector('.container-sheep');
+    function randomPosition() {
         const containerWidth = gameContainer.clientWidth;
         const containerHeight = gameContainer.clientHeight;
+        const sheepWidth = sheep.clientWidth;
+        const sheepHeight = sheep.clientHeight;
 
-        const randomX = Math.floor(Math.random() * (containerWidth - sheep.offsetWidth));
-        const randomY = Math.floor(Math.random() * (containerHeight - sheep.offsetHeight));
+        const randomX = Math.floor(Math.random() * (containerWidth - sheepWidth));
+        const randomY = Math.floor(Math.random() * (containerHeight - sheepHeight));
 
         sheep.style.left = `${randomX}px`;
         sheep.style.top = `${randomY}px`;
@@ -28,6 +31,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     sheep.addEventListener('mousedown', () => {
         sheep.style.display = 'none';
+        sheepsClicked++;
+
+        if (sheepsClicked >= totalSheep) {
+            gameContainer.classList.add('faded');
+        }
     });
 
     function moveSheep() {
