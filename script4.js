8df1cbf5ed50edc2bc38ce2ef527b641a0b0791e
EduardaBorgesD <medafbo@gmail.com>
2024-06-28 01:46:49 +0100

    Rascunhos e Background
    O gif antes da ovelha está implementado mas só aparece uma vez. O background final aparece após o primeiro clique e vai aumentando de opacidade à medida dos cliques.

--- script4.js
+++ script4.js
@@ -1,19 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sheep = document.getElementById('sheep');
+    const gifSheep = document.getElementById('ap_sheep');
+    const imgSheep = document.getElementById('i_sheep');
     let sheepTimer;
     let sheepsClicked = 0;
     const totalSheep = 15;
     const gameContainer = document.querySelector('.container-sheep');
-    const video = document.querySelector("#in-video video");
+    const video = document.querySelector(".ac1");
+    const video2 = document.querySelector(".ac2");
     const gamedisplay = document.getElementById("gamedisplay");
 
-    if (video) {
-        video.addEventListener('ended', () => {
+    setTimeout(() => {
+        video2.style.display = 'block';
+        video.style.display = 'none';
+    }, 4000);
+
+    setTimeout(() => {
+        if (video2.style.display === "block") {
             gamedisplay.style.display = 'block';
-            video.style.display = 'none';
             moveSheep();
-        });
-    }
+        }
+    }, 4000); 
 
     function randomPosition() {
         const containerWidth = gameContainer.clientWidth;
@@ -27,18 +34,31 @@ document.addEventListener('DOMContentLoaded', () => {
         sheep.style.left = `${randomX}px`;
         sheep.style.top = `${randomY}px`;
         sheep.style.display = 'block';
+
+        gifSheep.style.display = 'block';
+        imgSheep.style.display = 'none';
+
+        setTimeout(() => {
+            gifSheep.style.display = 'none';
+            imgSheep.style.display = 'block';
+        }, 1000);
     }
 
     sheep.addEventListener('mousedown', () => {
         sheep.style.display = 'none';
         sheepsClicked++;
 
-        if (sheepsClicked >= totalSheep) {
-            gameContainer.classList.add('faded');
-        }
+        const opacity = sheepsClicked / totalSheep;
+        gameContainer.classList.add('faded');
+        setTimeout(() => {
+            gameContainer.style.background = `linear-gradient(rgba(255,255,255,${1 - opacity}), rgba(255,255,255,${1 - opacity})), url('images/campo.jpg') no-repeat center center`;
+            gameContainer.style.backgroundSize = 'cover';
+        }, 50);
     });
 
     function moveSheep() {
-        sheepTimer = setInterval(randomPosition, 2000);
+        const inc_timer=500;
+        sheepsClicked = 0;
+        sheepTimer = setInterval(randomPosition, 4000-inc_timer);
     }
 });
