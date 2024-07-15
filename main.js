fee4419b31b1443a1e210a5c35104adf552cd57f
EduardaBorgesD <medafbo@gmail.com>
2024-07-15 01:07:13 +0100

    Olhos a Seguir Mouse

--- main.js
+++ main.js
@@ -13,33 +13,48 @@ function rotateHeads(event) {
 
 window.addEventListener('mousemove', rotateHeads);
 
-/* document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
+function eyeball(event) {
+    const eyes = document.querySelectorAll('.eye');
+    eyes.forEach(function(eye) {
+        let headContainer = eye.closest('.head-container');
+        if (!headContainer) return;
 
-    document.addEventListener('mousemove', (e) => {
-        const mouseX = e.clientX;
-        const mouseY = e.clientY;
-        const anchor = document.getElementById('anchor');
-        if (!anchor) {
-            console.error('Anchor element not found');
-            return; // Check if anchor is null
-        }
-        const rekt = anchor.getBoundingClientRect();
-        const anchorX = rekt.left + rekt.width / 2;
-        const anchorY = rekt.top + rekt.height / 2;
-        const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+        let containerRect = headContainer.getBoundingClientRect();
 
-        const eyes = document.querySelectorAll('.eye');
-        eyes.forEach(eye =>{
-            eye.style.transform = `rotate(${10+ angleDeg}deg)`;
-        })
+        let containerLeft = containerRect.left;
+        let containerTop = containerRect.top;
+        let containerWidth = containerRect.width;
+        let containerHeight = containerRect.height;
+
+        let eyeWidth = eye.clientWidth;
+        let eyeHeight = eye.clientHeight;
+
+        let maxX = containerWidth / 12 - eyeWidth / 2; // limite direito
+        let maxY = containerHeight / 26 - eyeHeight / 2; // limite inferior
+        let minX = -containerWidth / 24 + eyeWidth / 2; // limite esquerdo
+        let minY = -containerHeight / 34 + eyeHeight / 2; // limite superior
+
+        let eyeCenterX = containerLeft + containerWidth / 2;
+        let eyeCenterY = containerTop + containerHeight / 2;
+
+        let mouseX = event.pageX;
+        let mouseY = event.pageY;
+
+        let relativeX = mouseX - eyeCenterX;
+        let relativeY = mouseY - eyeCenterY;
+
+
+        if (relativeX > maxX) relativeX = maxX;
+        if (relativeX < minX) relativeX = minX;
+        if (relativeY > maxY) relativeY = maxY;
+        if (relativeY < minY) relativeY = minY;
+
+        let radian = Math.atan2(relativeY, relativeX);
+        let rotation = radian * (180 / Math.PI);
+        rotation *= 0.4;
+
+        eye.style.transform = `translate(${relativeX}px, ${relativeY}px) rotate(${rotation}deg)`;
     });
+}
 
-    function angle(cx, cy, ex, ey) {
-        const dy = ey - cy;
-        const dx = ex - cx;
-        const rad = Math.atan2(dy, dx);
-        const deg = rad * 180 / Math.PI;
-        return deg;
-    }
-}); */
+window.addEventListener('mousemove', eyeball);
